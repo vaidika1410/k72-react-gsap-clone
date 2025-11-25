448b2b57b4486a9c65ca8d6952bbf63ca51db866
vaidika1410 <vaidikakaul20@gmail.com>
2025-11-25 13:51:19 +0530

    change file path

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
         <video 
         className='h-full w-full object-cover'
         autoPlay muted loop 
-        src='public\k72-homepage-video.mp4' />
+        src='/k72-react-gsap-clone/video.mp4' />
     </div>
   )
 }
