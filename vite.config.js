7c3127e04a3fd2cf4570fd08f3e233e52e520d8a
vaidika1410 <vaidikakaul20@gmail.com>
2025-11-25 18:25:50 +0530

    update path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/k72-react-gsap-clone/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'index.html'),
+      }
+    }
+  }
 })
